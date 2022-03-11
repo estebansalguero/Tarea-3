2d3754c32651b9c66543ee2c815df901d1fb02c9
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2022-03-10 19:54:58 -0600

    Starting over

--- src/main/java/com/Tarea3/dao/PublicacionDao.java
+++
@@ -1,8 +0,0 @@
-package com.Tarea3.dao;
-
-import com.Tarea3.domain.Publicacion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublicacionDao extends CrudRepository<Publicacion, Long>{
-    
-}
